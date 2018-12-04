96100cd4d9d42c9b1032d7f64750d71fbbf74db1
pankaiya <3131225624@qq.com>
2018-12-04 16:36:02 +0800

    提交

---
+++ two-cylinder/src/pages/fabiao/fabiao.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FabiaoPage } from './fabiao';
+
+@NgModule({
+  declarations: [
+    FabiaoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FabiaoPage),
+  ],
+})
+export class FabiaoPageModule {}
