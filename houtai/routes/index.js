4bc28f771f375cc5d5b0c7a1dd0e257a9e7a8432
wangding <408542507@qq.com>
2018-12-05 16:13:46 +0800

    后台迭代

--- houtai/routes/index.js
+++ houtai/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',(req,res,next)=>{
-  res.render('login');
+  res.render('login',{flag:0});
 });
 router.get('/index', (req, res,next)=> {  
   res.render('index');
@@ -12,9 +12,8 @@ router.post('/',(req,res,next)=>{
   if(req.body.username=='hello' && req.body.password=='123456'){
     res.redirect('/index');
   }else{
-    res.render('login');
-    
+    res.render('login',{flag:1}); 
   }
-})
+});
 
 module.exports = router;
